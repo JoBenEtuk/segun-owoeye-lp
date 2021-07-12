c28391b754955e270fcaf8262dc80e0c30007ec2
JoBenEtuk <JoBenEtuk@users.noreply.github.com>
2021-07-12 10:28:19 +0100

    service header

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -22,14 +22,10 @@ const Hero = () => {
     }
   `)
     const data = query.allContentfulHomePage.nodes[0].introduction
-    console.log(data)
+
     return (
-        <div className="home-hero">
+        <div className="home-hero" style={{ backgroundImage: `linear-gradient(rgba(10, 10, 10, 0.5) 100%, rgba(10, 10, 10, 0.5)100%), url(https:${data.backgroundImage.fluid.src})` }}>
             <div className="container">
-                <img
-                    src={`https:${data.backgroundImage.fluid.src}`}
-                    alt="background image"
-                />
                 <h1>{data.title}</h1>
                 <h3>{data.description.description}</h3>
                 <button>Schedule Appointment</button>
