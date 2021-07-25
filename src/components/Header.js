2c9b2950610ad3854bfde5d60b164b3a48ca2504
JoBenEtuk <JoBenEtuk@users.noreply.github.com>
2021-07-25 23:31:14 +0100

    className update

--- src/components/Header.js
+++ src/components/Header.js
@@ -116,7 +116,7 @@ const Header = () => {
                                 height={16}
                             />
                         </div>
-                        <svg role="presentation" onClick={() => { setOpen(true) }} xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg role="presentation" onClick={() => { setOpen(true) }} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
                     </div>
@@ -124,7 +124,7 @@ const Header = () => {
                     {open &&
                         <nav>
                             <div className="container">
-                                <svg role="presentation" onClick={() => { setOpen(false) }} xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <svg role="presentation" onClick={() => { setOpen(false) }} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                                 </svg>
                                 <ul>
