1810eadd76276d28d06bc488f0e773bb44ba5a67
JoBenEtuk <JoBenEtuk@users.noreply.github.com>
2021-07-25 16:33:56 +0100

    services refactor

--- src/components/Service.js
+++ src/components/Service.js
@@ -44,9 +44,6 @@ const Service = () => {
                   <img src={`https:${item.icon.fluid.src}`} alt="icon" />
                   <h4>{item.title}</h4>
                   <p>{item.description.description}</p>
-                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9 18L15 12L9 6" stroke="#5B4700" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                  </svg>
                 </li>
               ))}
             </ul>
@@ -54,7 +51,6 @@ const Service = () => {
             <div className="flex-ac-jc">
               <button className="btn-main">Schedule Appointment</button>
             </div>
-            <h1>{data.teamTitle}</h1>
           </div>
         </div>
       </div>
