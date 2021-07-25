ebec667824949ce16f3ec45f9bfae6db5bacba21
JoBenEtuk <JoBenEtuk@users.noreply.github.com>
2021-07-25 19:33:18 +0100

    nav mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react'
+import { useMediaQuery } from "../hooks"
 import { useStaticQuery, graphql, navigate } from "gatsby"
 
 const Header = () => {
+    const isTablet = useMediaQuery("(min-width: 913px)")
+    const [open, setOpen] = useState(false)
     const query = useStaticQuery(graphql`
       {
         allContentfulHomePage {
@@ -54,29 +57,65 @@ const Header = () => {
     }, [])
 
     return (
-        <header className="header">
-            <div className="container flex-ac-jb">
-                <div className="header__left">
-                    <img
-                        src={`https:${data}`}
-                        alt="logo"
-                    />
+        <>
+            {isTablet && <header className="header">
+                <div className="container flex-ac-jb">
+                    <div className="header__left">
+                        <img
+                            src={`https:${data}`}
+                            alt="logo"
+                        />
+                    </div>
+                    <nav className="header__right">
+                        <ul>
+                            {links.map((link, index) => (
+                                <li key={index}>
+                                    <a href={link.url} className={active[index] ? "active" : ""} onClick={() => handleActive(index)}>
+                                        <span>{link.title}</span>
+                                    </a>
+                                </li>
+
+                            ))}
+                        </ul>
+                    </nav>
+
                 </div>
-                <nav className="header__right">
-                    <ul>
-                        {links.map((link, index) => (
-                            <li key={index}>
-                                <a href={link.url} className={active[index] ? "active" : ""} onClick={() => handleActive(index)}>
-                                    <span>{link.title}</span>
-                                </a>
-                            </li>
+            </header>}
 
-                        ))}
-                    </ul>
-                </nav>
+            {!isTablet && <header className="header mobile">
+                <div className="container">
+                    <div className="header__top flex-ac-jb">
+                        <img
+                            src={`https:${data}`}
+                            alt="logo"
+                        />
+                        <svg onClick={() => { setOpen(true) }} xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>
+                    </div>
 
-            </div>
-        </header>
+                    {open &&
+                        <nav>
+                            <div className="container">
+                                <svg onClick={() => { setOpen(false) }} xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                                <ul>
+                                    {links.map((link, index) => (
+                                        <li key={index} onClick={() => { setOpen(false) }}>
+                                            <a href={link.url} className={active[index] ? "active" : ""} onClick={() => handleActive(index)}>
+                                                <span>{link.title}</span>
+                                            </a>
+                                        </li>
+
+                                    ))}
+                                </ul>
+                            </div>
+                        </nav>}
+
+                </div>
+            </header>}
+        </>
     )
 }
 
