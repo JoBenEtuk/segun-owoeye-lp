066532731a943a7a591657b03fd6b0670c44240f
JoBenEtuk <JoBenEtuk@users.noreply.github.com>
2021-07-26 15:27:01 +0100

    team fail

--- src/components/Details.js
+++ src/components/Details.js
@@ -23,7 +23,7 @@ const Details = () => {
     return (
         <aside className="details">
             <section className="container flex-ac-jb">
-                <div className="details__left">
+                <address className="details__left">
                     <span>
                         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M2.66659 2.66666H13.3333C14.0666 2.66666 14.6666 3.26666 14.6666 3.99999V12C14.6666 12.7333 14.0666 13.3333 13.3333 13.3333H2.66659C1.93325 13.3333 1.33325 12.7333 1.33325 12V3.99999C1.33325 3.26666 1.93325 2.66666 2.66659 2.66666Z" stroke="#806F36" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -45,7 +45,7 @@ const Details = () => {
                         </svg>
                         {data.phoneNumber}
                     </span>
-                </div>
+                </address>
 
                 <nav className="details__right">
                     <a aria-label="twitter" href={data.twitterUrl} rel="noreferrer noopenner" target="_blank">
